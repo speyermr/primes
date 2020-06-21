b034bb2aa84a41de60946796f590f5d363383da5
Ed <ededededed.edededede@ededededed.ededededededededed>
2020-06-21 19:20:36 +0100

    Renaming files

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 import time  # Used in test_speed()
 
 # Our six prime number libraries:
-import primes.basic
-import primes.shortcircuit
-import primes.mod
-import primes.inner
-import primes.squarestop
-import primes.sieve
+import primes.v0_basic
+import primes.v1_shortcircuit
+import primes.v2_mod
+import primes.v3_inner
+import primes.v4_squarestop
+import primes.v5_sieve
 
 # Test that the code works
 def correctness_test():
-    print( list( primes.basic.upto(10) ))
-    print( list( primes.shortcircuit.upto(10)))
-    print( list( primes.mod.upto(10)))
-    print( list( primes.inner.upto(10)))
-    print( list( primes.squarestop.upto(10)))
-    print( list( primes.sieve.upto(10)))
+    print( list( primes.v0_basic.upto(10) ))
+    print( list( primes.v1_shortcircuit.upto(10)))
+    print( list( primes.v2_mod.upto(10)))
+    print( list( primes.v3_inner.upto(10)))
+    print( list( primes.v4_squarestop.upto(10)))
+    print( list( primes.v5_sieve.upto(10)))
 
 def test_speed():
     # A list of all the modules we've made
     modules = (
-            primes.basic,
-            primes.shortcircuit,
-            primes.mod,
-            primes.inner,
-            primes.squarestop,
-            primes.sieve,
+            primes.v0_basic,
+            primes.v1_shortcircuit,
+            primes.v2_mod,
+            primes.v3_inner,
+            primes.v4_squarestop,
+            primes.v5_sieve,
             )
 
     for limit in [16, 32, 64, 128, 256, 512]:
         print(f'Finding all primes up to {limit}...')
         for module in modules:
-            expected = list(primes.sieve.upto(limit))
+            expected = list(primes.v5_sieve.upto(limit))
             t0 = time.time_ns()
             actual = list(module.upto(limit))
             t1 = time.time_ns()
